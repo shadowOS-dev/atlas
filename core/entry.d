462406bbdc7eda5aa02150bca3c98a5c73994100
Kevin Alavik <kevin@alavik.se>
2025-04-01 09:50:04 +0200

    feat: Kinda got modules working?

--- core/entry.d
+++ core/entry.d
@@ -11,6 +11,28 @@ module core.entry;
 import util.cpu;
 import dev.portio;
 import lib.printf;
+import core.limine;
+
+/* Limine stuff */
+mixin LIMINE_LINKER_DIRECTIVE;
+immutable ulong[3] limine_base_revision = LIMINE_BASE_REVISION!3;
+
+// Framebuffer request
+mixin LIMINE_LINKER_DIRECTIVE;
+static immutable framebuffer_request = limine_framebuffer_request(
+    LIMINE_FRAMEBUFFER_REQUEST, 0, null);
+
+// Requests start and end
+// __gshared pragma(linkerDirective, "used, section=.limine_requests_start");
+// extern (C) __gshared ulong[4] limine_requests_start_marker = [
+//     0xf6b8f4b39de7d1ae, 0xfab91a6940fcb9cf,
+//     0x785c6ed015d3e316, 0x181e920a7852b9d9
+// ];
+
+// __gshared pragma(linkerDirective, "used, section=.limine_requests_end");
+// extern (C) __gshared ulong[2] limine_requests_end_marker = [
+//     0xadc0e0531bb10d03, 0x9572709f31764c62
+// ];
 
 /* Utility functions */
 extern (C) void __assert(const(char)* msg, const(char)* file, uint line)
@@ -22,6 +44,19 @@ extern (C) void __assert(const(char)* msg, const(char)* file, uint line)
 /* Entry point */
 extern (C) void kmain()
 {
+    foreach (i, ulong v; limine_base_revision)
+    {
+        printf("%d: 0x%.16llx\n", i, v);
+    }
+    assert(limine_base_revision[2] == 0, "Unsupported limine base revision");
     printf("Atlas kernel v1.0-alpha\n");
+    if (framebuffer_request.response == null || framebuffer_request.response.framebuffer_count <= 0)
+    {
+        printf("no framebuffer :^(\n");
+        hcf();
+    }
+
+    printf("we got %d framebuffer(s)!\n", framebuffer_request.response.framebuffer_count);
+    printf("response rev: %d\n", framebuffer_request.response.revision);
     halt();
 }
--- core/limine.d
+++ core/limine.d
@@ -8,4 +8,76 @@ module core.limine;
  * Date: March 31, 2025
  */
 
-// Limine v9.x
+/* Misc */
+enum ulong LIMINE_COMMON_MAGIC_1 = 0xc7b1dd30df4c8b88;
+enum ulong LIMINE_COMMON_MAGIC_2 = 0x0a82e883a194f07b;
+
+template LIMINE_BASE_REVISION(uint N)
+{
+    enum ulong[3] LIMINE_BASE_REVISION = [
+            0xf9562b2d5c95a6c8, 0x6a7b384944536bdc, N
+        ];
+}
+
+mixin template LIMINE_LINKER_DIRECTIVE()
+{
+    __gshared pragma(linkerDirective, "used, section=.limine_requests");
+}
+
+/* Framebuffer */
+enum LIMINE_FRAMEBUFFER_RGB = 1;
+enum LIMINE_FRAMEBUFFER_REQUEST = [
+        LIMINE_COMMON_MAGIC_1, LIMINE_COMMON_MAGIC_2, 0x9d5827dcd881dd75,
+        0xa3148604f6fab11b
+    ];
+
+struct limine_video_mode
+{
+    ulong pitch;
+    ulong width;
+    ulong height;
+    ushort bpp;
+    ubyte memory_model;
+    ubyte red_mask_size;
+    ubyte red_mask_shift;
+    ubyte green_mask_size;
+    ubyte green_mask_shift;
+    ubyte blue_mask_size;
+    ubyte blue_mask_shift;
+}
+
+struct limine_framebuffer
+{
+    void* address;
+    ulong width;
+    ulong height;
+    ulong pitch;
+    ushort bpp;
+    ubyte memory_model;
+    ubyte red_mask_size;
+    ubyte red_mask_shift;
+    ubyte green_mask_size;
+    ubyte green_mask_shift;
+    ubyte blue_mask_size;
+    ubyte blue_mask_shift;
+    ubyte[7] unused;
+    ulong edid_size;
+    void* edid;
+    /* Response revision 1 */
+    ulong mode_count;
+    limine_video_mode*[] modes;
+}
+
+struct limine_framebuffer_response
+{
+    ulong revision;
+    ulong framebuffer_count;
+    limine_framebuffer*[] framebuffers;
+}
+
+struct limine_framebuffer_request
+{
+    ulong[4] id;
+    ulong revision;
+    limine_framebuffer_response* response;
+}
