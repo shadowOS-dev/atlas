edf521b98fe6e109a8d67f147afa514399b0d7a6
Kevin Alavik <kevin@alavik.se>
2025-04-03 14:54:18 +0200

    fix: Fixed IDT

--- sys/idt.d
+++ sys/idt.d
@@ -51,7 +51,7 @@ align(1):
 __gshared align(16) IDTEntry[256] idt = IDTEntry(0);
 extern (C) alias IDTIntrHandler = void function(RegisterCtx* refs);
 extern (C) __gshared IDTIntrHandler[256] realHandlers = void;
-extern (C) extern __gshared ulong[] stubs;
+extern (C) extern __gshared ulong[256] stubs;
 __gshared IDTPointer idtPtr = IDTPointer(0);
 
 void setGate(int interrupt, ulong base, ubyte flags)
@@ -61,21 +61,19 @@ void setGate(int interrupt, ulong base, ubyte flags)
         kprintf("Invalid interrupt number: %d", interrupt);
         return;
     }
-    
-    idt[interrupt] = IDTEntry(
-        cast(ushort)(base & 0xFFFF),
-        0x08,
-        0,
-        flags,
-        cast(ushort)((base >> 16) & 0xFFFF),
-        cast(uint)((base >> 32) & 0xFFFFFFFF),
-        0
-    );
+
+    idt[interrupt].offLow = (base) & 0xFFFF;
+    idt[interrupt].sel = 0x8;
+    idt[interrupt].ist = 0;
+    idt[interrupt].attr = flags;
+    idt[interrupt].offMid = ((base) >> 16) & 0xFFFF;
+    idt[interrupt].offHigh = ((base) >> 32) & 0xFFFFFFFF;
+    idt[interrupt].zero = 0;
 }
 
 void handleInterrupt(RegisterCtx* ctx)
 {
-    assert(null, "panic");
+    assert(null, "kernel panic");
 }
 
 void initIDT()
@@ -84,11 +82,12 @@ void initIDT()
     kprintf("IDTPointer.limit: %d", idtPtr.limit);
     idtPtr.base = cast(ulong)(&idt);
     kprintf("IDTPointer.base: 0x%.16llx", idtPtr.base);
+    kprintf("stubs base address: 0x%.16llx", cast(ulong)&stubs);
+
     foreach (i; 0 .. 32)
     {
-        setGate(i, stubs[i], IDT_TRAP_GATE);
         realHandlers[i] = cast(IDTIntrHandler)&handleInterrupt;
-        kprintf("realHandlers[%d] -> = 0x%.16llx", i, cast(ulong) realHandlers[i]);
+        setGate(i, cast(ulong) stubs[i], IDT_TRAP_GATE);
     }
 
     foreach (i; 32 .. 256)
